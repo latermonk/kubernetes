b7c61968ff4eee636692f969b808e4c6d395d00c
latermonk <sprout_wang@163.com>
2018-03-25 22:07:04 +0800

    update

--- k8s_install_script.sh
+++ k8s_install_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#echo "Hello world"
 
-#swapoff  -a
-
+echo "Hello world"
 echo hello
 if [ $?  != 0 ]
 then 
@@ -11,4 +9,20 @@ else
 	echo "sucess !"
 fi
 
+# start script
+
+# 关闭swap
+swapoff  -a
+
+
+
+```
+考虑几个问题：
+
+如何在脚本运行关键语句不正确时停止脚本的运行？
+
+如何在脚本运行非关键语句不正确的时候忽略错误继续执行，但是在脚本执行的最后对错误进行告警？
+
+
+```
 
